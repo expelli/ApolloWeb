77244e40e277fa53db03da0072f0ec442eabe8cc
Selina Niederlaender <S1810307095@students.fh-hagenberg.at>
2021-01-24 00:51:06 +0100

    changed a bit in genre management

--- src/app/genre-management/genre-management.component.ts
+++ src/app/genre-management/genre-management.component.ts
@@ -14,36 +14,49 @@ export class GenreManagementComponent implements OnInit {
   @ViewChild('addForm', {static: true}) addForm!: NgForm;
 
   constructor(private genreService: GenreService, private snackBar: MatSnackBar) {
+    this.initInputs();
+  }
+  initInputs() {
     this.newGenre= new Genre(), 
     this.delGenre= new Genre(),  
     this.updGenre= new Genre()
   }
-
   genres!: Genre[];
 
-  newGenre: Genre;
-  delGenre: Genre;
-  updGenre: Genre;
+  newGenre!: Genre;
+  delGenre!: Genre;
+  updGenre!: Genre;
 
   ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData(){
     this.genreService.getAllGenres().subscribe(res => this.genres = res);
-    ;
+    this.initInputs();
   }
 
   addGenre(){
     this.genreService.addGenre(this.newGenre).subscribe();
     this.showSnackBar(""+this.newGenre.name+" "+this.newGenre.id,"")
-    //this.addForm.reset(this.newGenre);
+    this.loadData();
   }
+
   deleteGenre(){
     this.genreService.deleteGenre(this.delGenre.id).subscribe();
-    this.showSnackBar(""+this.delGenre.id,"")
+    this.showSnackBar(""+this.delGenre.id,"");
+    this.loadData();
     //this.addForm.reset(this.newGenre);
   }
   updateGenre(){
     this.genreService.updateGenre(this.updGenre).subscribe();
-    this.showSnackBar(""+this.delGenre.id,"")
+    this.showSnackBar(""+this.delGenre.id,"");
+    this.loadData();
     //this.addForm.reset(this.newGenre);
+  }
+
+  newGenreNotProperyDefined():boolean {
+    return (!this.newGenre.id) || (!this.newGenre.name) || (this.newGenre.id =='') ||  (this.newGenre.name =='');
   }
 
   delGenreChanged(value: any){
